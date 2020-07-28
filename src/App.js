aee88cd56ec936106552ec5adf372308acb87c12
Warren Uhrich <hello@warren.codes>
2020-07-28 11:15:54 -0600

    Add action values; add logic/returns for to-do reducer.

--- src/App.js
+++ src/App.js
@@ -8,14 +8,16 @@ import { v4 as uuidv4 } from 'uuid';
  * The action "names" ('type' property values), by convention, are
  * uppercase as they are representing a "constant" value.
  */
-const addNewToDo = () => {
+const addNewToDo = toDoContent => {
   return {
-    type: 'ADD_NEW_TO_DO'
+    type: 'ADD_NEW_TO_DO', // Our action "label."
+    value: toDoContent // We can also transport necessary info that the reducer might need.
   }
 }
-const removeToDo = () => {
+const removeToDo = toDoId => {
   return {
-    type: 'REMOVE_TO_DO'
+    type: 'REMOVE_TO_DO', // Our action "label."
+    value: toDoId // For removal, we need a unique identifier.
   }
 }
 
@@ -25,13 +27,26 @@ const removeToDo = () => {
  * state data. It should expect an "action" to be passed in with any
  * necessary target data to perform its duty.
  */
-const toDoReducer = ( state, action ) => {
+const toDoReducer = ( state = [], action ) => { // Default state is an empty array here.
   switch ( action.type )
   {
+    // What happens if we are adding a new to-do:
     case 'ADD_NEW_TO_DO':
-
+      // Set up new task.
+      const newTask = {
+        uniqueId: uuidv4(), // Ensure a unique ID.
+        value: action.value // Read passed-in "new to-do" value.
+      };
+      // Add this task to the state.
+      state.push( newTask );
+      // Return the updated state value.
+      return state;
+    // What happens if we are removing an existing to-do:
     case 'REMOVE_TO_DO':
-      
+      // Returns a filtered version of the array, leaving only the items that DIDN'T match the "id" parameter.
+      state = state.filter( toDo => toDo.uniqueId !== action.value ); // We'll have an array without the target!
+      // Return the updated state value.
+      return state;
   }
 }
 
